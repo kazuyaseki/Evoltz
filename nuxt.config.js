c049e7448c40c90f69ed10c8d2189f4e1ffcd8fc
kazuya-seki <track613armory@gmail.com>
2018-02-17 13:58:40 +0900

    added normalize.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,46 @@
-const parseArgs = require('minimist')
+const parseArgs = require("minimist");
 const argv = parseArgs(process.argv.slice(2), {
   alias: {
-    H: 'hostname',
-    p: 'port'
+    H: "hostname",
+    p: "port"
   },
-  string: ['H'],
+  string: ["H"],
   unknown: parameter => false
-})
+});
 
 const port =
   argv.port ||
   process.env.PORT ||
   process.env.npm_package_config_nuxt_port ||
-  '3000'
+  "3000";
 const host =
   argv.hostname ||
   process.env.HOST ||
   process.env.npm_package_config_nuxt_host ||
-  'localhost'
+  "localhost";
 module.exports = {
   env: {
     baseUrl: process.env.BASE_URL || `http://${host}:${port}`
   },
   head: {
-    title: 'evoltz',
+    title: "evoltz",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
-  css: ['tachyons/css/tachyons.min.css', '~/assets/css/main.css'],
+  css: ["~/assets/css/normalize.css", "~/assets/css/main.css"],
   build: {
-    vendor: ['axios', 'gsap', 'vuex-class', 'nuxt-class-component']
+    vendor: ["axios", "gsap", "vuex-class", "nuxt-class-component"]
   },
-  modules: ['~/modules/typescript.js']
-}
+  modules: ["~/modules/typescript.js"]
+};
