9462a32da0ff9811a33bf1abe53ec8aeb0ae0587
kazuya-seki <track613armory@gmail.com>
2018-02-17 13:59:10 +0900

    added projects col

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="flex">
-    <ul class="todos">
-      <li :key="todo" v-for="todo in todos">{{ todo }}</li>
-    </ul> 
-  </div>
+  <main>
+    <div class="projects">
+      <ul>
+        <li :key="project.id" v-for="project of projects">
+          <span class="project-icon" :style="{ backgroundColor: project.color }"></span>
+          {{ project.name }}
+        </li>
+      </ul>
+    </div>
+    <div class="todos">
+      <ul>
+        <li :key="todo" v-for="todo in todos">{{ todo }}</li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -15,6 +25,11 @@ import Component from "nuxt-class-component";
 })
 export default class extends Vue {
   todos: string[] = ["買い物", "洗濯", "掃除"];
+  projects: { id: string; name: string; color: string }[] = [
+    { id: "hoge", name: "仕事", color: "red" },
+    { id: "hogee", name: "生産性向上", color: "blue" },
+    { id: "hogeee", name: "家事", color: "yellow" }
+  ];
 }
 </script>
 <style lang="scss" scoped>
