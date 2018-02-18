9f0e73314ec94e6b81de1eeb44ff7dce7590afdd
kazuya-seki <track613armory@gmail.com>
2018-02-18 20:35:09 +0900

    created ProjectSidebar component

---
+++ components/ProjectSidebar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="projects">
+    <ul>
+      <li :key="project.id" v-for="project of projects">
+        <span class="project-icon" :style="{ backgroundColor: project.color }"></span>
+        {{ project.name }}
+      </li>
+    </ul>
+    <div v-if="addingProject">
+      <input type="text" v-model="projectName" />
+      <button @click="addProject">プロジェクトを追加する</button>
+      <button @click="setAddingProjectMode(false)">キャンセル</button>
+    </div>
+    <button v-else @click="setAddingProjectMode(true)" class="add-project-button">+ プロジェクトを追加</button>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class extends Vue {
+  @Prop() projects: { id: string; name: string; color: string }[];
+
+  projectName: string = "";
+  addingProject: boolean = false;
+
+  setAddingProjectMode(newMode) {
+    this.addingProject = newMode;
+  }
+
+  addProject() {
+    this.projects.push({ id: "hogeead", name: this.projectName, color: "red" });
+    this.setAddingProjectMode(false);
+  }
+}
+</script>
+<style lang="scss" scoped>
+.projects {
+  grid-area: g0;
+  font-size: 14px;
+  ul {
+    list-style: none;
+    padding: 0px;
+    li {
+      padding: 8px 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: #fff;
+      }
+    }
+  }
+}
+.project-icon {
+  width: 12px;
+  height: 12px;
+  display: inline-block;
+  border-radius: 50%;
+  margin-right: 8px;
+}
+.add-project-button {
+  border: none;
+  background-color: inherit;
+  color: #dd4b39;
+  cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,6 @@
 <template>
   <main>
-    <div class="projects">
-      <ul>
-        <li :key="project.id" v-for="project of projects">
-          <span class="project-icon" :style="{ backgroundColor: project.color }"></span>
-          {{ project.name }}
-        </li>
-      </ul>
-      <div v-if="addingProject">
-        <input type="text" v-model="projectName" />
-        <button @click="addProject">プロジェクトを追加する</button>
-        <button @click="setAddingProjectMode(false)">キャンセル</button>
-      </div>
-      <button v-else @click="setAddingProjectMode(true)" class="add-project-button">+ プロジェクトを追加</button>
-    </div>
+    <ProjectSidebar :projects="projects"/>
     <div class="todos">
       <ul>
         <li :key="todo" v-for="todo in todos">{{ todo }}</li>
@@ -26,8 +13,12 @@
 import Vue from "vue";
 import Component from "nuxt-class-component";
 
+import ProjectSidebar from "../components/ProjectSidebar.vue";
+
 @Component({
-  components: {}
+  components: {
+    ProjectSidebar
+  }
 })
 export default class extends Vue {
   todos: string[] = ["買い物", "洗濯", "掃除"];
@@ -36,17 +27,6 @@ export default class extends Vue {
     { id: "hogee", name: "生産性向上", color: "blue" },
     { id: "hogeee", name: "家事", color: "yellow" }
   ];
-  projectName: string = "";
-  addingProject: boolean = false;
-
-  setAddingProjectMode(newMode) {
-    this.addingProject = newMode;
-  }
-
-  addProject() {
-    this.projects.push({ id: "hogeead", name: this.projectName, color: "red" });
-    this.setAddingProjectMode(false);
-  }
 }
 </script>
 <style lang="scss" scoped>
@@ -59,36 +39,6 @@ main {
   grid-template-rows: 1fr;
   grid-template-areas: "g0 g1";
 }
-.projects {
-  grid-area: g0;
-  font-size: 14px;
-  ul {
-    list-style: none;
-    padding: 0px;
-    li {
-      padding: 8px 10px;
-      cursor: pointer;
-      &:hover {
-        background-color: #fff;
-      }
-    }
-  }
-}
-.project-icon {
-  width: 12px;
-  height: 12px;
-  display: inline-block;
-  border-radius: 50%;
-  margin-right: 8px;
-}
-
-.add-project-button {
-  border: none;
-  background-color: inherit;
-  color: #dd4b39;
-  cursor: pointer;
-}
-
 .todos {
   grid-area: g1;
   background-color: #fff;
