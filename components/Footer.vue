89f389a5b1d1d8b39b03d0a4e392ed4a2868c78e
kazuya-seki <track613armory@gmail.com>
2018-02-23 22:11:08 +0900

    Footer コンポーネントを追加

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <div>タイマー予定地</div>
+  </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  border-top: 1px solid #666;
+  background-color: #000;
+  color: #fff;
+  padding: 8px 10px;
+  box-sizing: border-box;
+
+  z-index: 1px;
+}
+</style>
