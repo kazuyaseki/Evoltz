0c097823ebf31dca737e0c1d7e4b3b21522c9a67
kazuya-seki <track613armory@gmail.com>
2018-02-27 11:16:58 +0900

    added reflection component

---
+++ pages/reflection.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    Reflection
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+import { Types } from "../interface/types";
+
+@Component({
+  components: {}
+})
+export default class extends Vue {}
+</script>
+<style lang="scss" scoped>
+main {
+  margin: 0 auto;
+  max-width: 1080px;
+  min-height: 100%;
+  display: grid;
+  grid-template-columns: 2fr 5fr;
+  grid-template-rows: 1fr;
+  grid-template-areas: "g0 g1";
+}
+</style>
