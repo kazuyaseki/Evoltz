eae3abf7736085c86e9504d99eea1e22143bdadf
kazuya-seki <track613armory@gmail.com>
2018-03-14 21:05:23 +0900

    added new doneProject component

---
+++ components/doneProject.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>{{projectName}}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["projectName"],
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
