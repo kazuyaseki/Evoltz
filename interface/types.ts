48c81ec083a7d92d4ae872a76fc6e1cfe0bb5a02
kazuya-seki <track613armory@gmail.com>
2018-03-18 21:34:16 +0900

    modified type of project and todo to include id

--- interface/types.ts
+++ interface/types.ts
@@ -5,12 +5,14 @@ export namespace Types {
   }
 
   export interface Project {
+    id: string;
     name: string;
     color: string;
     todos: Todo[];
   }
 
   export interface Todo {
+    id: string;
     name: string;
     memo: string;
     completed: boolean;
